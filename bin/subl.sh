4254fb483d30ec3f42ae73125719946b97581823
uuutee <uuuteenet@gmail.com>
2016-06-22 01:12:06 +0900

    Add custom subl command

---
+++ bin/subl.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+# ディレクトリにマッチしたら終了
+if [[ $1 =~ /$ ]]; then
+	echo ${1} 'is directory.';
+
+# 存在するファイルなら開く
+elif [ -e $1 ]; then
+	/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl $1;
+
+# 存在しないファイルなら新規作成して開く
+else
+	while true; do
+		read -p 'file is not fount. do you make file? (y/n) ' yn
+		case $yn in
+			[Yy]* ) 
+				DIR=`dirname ${1}`;
+				mkdir -p ${DIR};
+				touch $1;
+				/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl $1;
+				break;;
+			[Nn]* ) 
+				echo 'not making'; 
+				exit;;
+			* ) echo 'Please answer yes or no.';;
+		esac
+	done
+fi
+
+exit 0
--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,3 +2,5 @@
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+chmod +x ~/dotfiles/bin/subl.sh
+ln -s ~/dotfiles/bin/subl.sh /usr/local/bin/subl
