22a6f3198970c7ec45c4d4710b9320671773ec9d
uuutee <uuuteenet@gmail.com>
2016-12-29 16:58:05 +0900

    Add update_files function

--- bin/update_files.sh
+++ bin/update_files.sh
@@ -1,8 +1,20 @@
 #! /bin/bash
-TARGET_PATH=cwp/wp-includes/class-phpmailer.php;
-FROM_DIR=~/Dropbox/dev/designinc/files/update/public_html/domain;
-SEARCH_DIR=~/Downloads/test/public_html;
 
-for file in `find ${SEARCH_DIR} -path ${TARGET_PATH}`; do 
-	ls -l ${file};
-done | xargs echo
+FROM_DIR=~/Dropbox/dev/designinc/files/update/public_html/domain;
+TARGET_PATH=/cwp/wp-includes/class-phpmailer.php;
+PUBLIC_HTML_DIR=~/Dropbox/dev/designinc/files/test/public_html;
+BACKUP_DIR=~/Dropbox/dev/designinc/files/backup;
+
+for website in `ls -l ${PUBLIC_HTML_DIR} | grep ^drw | awk '{print $9}'`; do 
+	cd "${PUBLIC_HTML_DIR}/${website}"
+
+	# 対象ファイルが見つかれば、backupして、上書き
+	for file in `find ./ -path *${TARGET_PATH}`; do 
+		# backup
+		mkdir -p "${BACKUP_DIR}/${website}"
+		cp -p ${file} "${BACKUP_DIR}/${website}"
+
+		# 上書き
+		cp -f "${FROM_DIR}${TARGET_PATH}" ${file}
+	done
+done
