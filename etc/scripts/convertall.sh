99a89701776e2bb4516f11e5b61f4c92fd9d768c
uuutee <uuuteenet@gmail.com>
2017-10-10 17:55:26 +0900

    Add convertall

---
+++ etc/scripts/convertall.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+# カレントディレクトリ内の画像を一括で指定フォーマットに変換する
+
+from_ext=$1
+dest_ext=$2
+
+images=($(find . -name "*.${from_ext}"))
+dest="./convert"
+
+# ディレクトリがなければ作成
+if [[ ! -e ${dest} ]]; then
+  mkdir -p ${dest}
+fi
+
+for image in ${images[@]}; do
+  # ファイル名のみを取得
+  name=$(basename ${image})
+  name=${name%.*}
+
+  # imagemagickで変換
+  convert ${image} -quality 100 ${dest}/${name}.${dest_ext}
+done
+
+exit 0
