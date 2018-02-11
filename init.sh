485b57775542fe70c82417c831c36a7204f09980
uuutee <uuuteenet@gmail.com>
2018-02-11 17:41:06 +0900

    Add gitconfig

--- init.sh
+++ init.sh
@@ -3,4 +3,6 @@ ln -s ~/.ghq/github.com/uuutee/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.ghq/github.com/uuutee/dotfiles/.bashrc ~/.bashrc
 ln -s ~/.ghq/github.com/uuutee/dotfiles/.vim ~/.vim
 ln -s ~/.ghq/github.com/uuutee/dotfiles/.vimrc ~/.vimrc
+ln -s ~/.ghq/github.com/uuutee/dotfiles/.gitconfig ~/.gitconfig
+ln -s ~/.ghq/github.com/uuutee/dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/.ghq/github.com/uuutee/atom/ ~/.atom
