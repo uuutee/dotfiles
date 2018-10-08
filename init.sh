0bec664dcfffa9e75f3372b6b93660ac703c4919
uuutee <uuuteenet@gmail.com>
2018-10-08 21:45:15 +0900

    Add vscode settings

--- init.sh
+++ init.sh
@@ -7,3 +7,9 @@ ln -sf ${SCRIPT_DIR}/.vim ~/.vim
 ln -sf ${SCRIPT_DIR}/.vimrc ~/.vimrc
 ln -sf ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
 ln -sf ${SCRIPT_DIR}/.gitignore_global ~/.gitignore_global
+
+# vscode
+if [[ -e "$HOME/Library/Application Support/Code/User/" ]]; then
+    ln -sf ${SCRIPT_DIR}/vscode/keybindings.json "$HOME/Library/Application Support/Code/User/keybindings.json"
+    ln -sf ${SCRIPT_DIR}/vscode/settings.json "$HOME/Library/Application Support/Code/User/settings.json"
+fi
