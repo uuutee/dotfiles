37bbf9bb3937fe6423d4645f37e872e0a3a08d3d
uuutee <uuuteenet@gmail.com>
2018-10-08 14:13:29 +0900

    Add force option

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
-ln -s -f ${SCRIPT_DIR}/.bash_profile ~/.bash_profile
-ln -s -f ${SCRIPT_DIR}/.bashrc ~/.bashrc
-ln -s -f ${SCRIPT_DIR}/.vim ~/.vim
-ln -s -f ${SCRIPT_DIR}/.vimrc ~/.vimrc
-ln -s -f ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
-ln -s -f ${SCRIPT_DIR}/.gitignore_global ~/.gitignore_global
+ln -sf ${SCRIPT_DIR}/.bash_profile ~/.bash_profile
+ln -sf ${SCRIPT_DIR}/.bashrc ~/.bashrc
+ln -sf ${SCRIPT_DIR}/.vim ~/.vim
+ln -sf ${SCRIPT_DIR}/.vimrc ~/.vimrc
+ln -sf ${SCRIPT_DIR}/.gitconfig ~/.gitconfig
+ln -sf ${SCRIPT_DIR}/.gitignore_global ~/.gitignore_global
