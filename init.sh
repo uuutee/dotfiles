be74209c5d3a1ca14be8ed1c47d1547c2ccdf5fd
uuutee <uuuteenet@gmail.com>
2018-10-08 22:15:12 +0900

    Fix dup symlink bug

--- init.sh
+++ init.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
+# file
 ln -sf ${SCRIPT_DIR}/.bash_profile $HOME/.bash_profile
 ln -sf ${SCRIPT_DIR}/.bashrc $HOME/.bashrc
-ln -sf ${SCRIPT_DIR}/.vim $HOME/.vim
 ln -sf ${SCRIPT_DIR}/.vimrc $HOME/.vimrc
 ln -sf ${SCRIPT_DIR}/.gitconfig $HOME/.gitconfig
 ln -sf ${SCRIPT_DIR}/.gitignore_global $HOME/.gitignore_global
+
+# directory (2重にsymlinkを作らないようにする)
+if [[ ! -L "$HOME/.vim" ]]; then
+    ln -s ${SCRIPT_DIR}/.vim $HOME/.vim
+fi
 
 # molokai.vim
 (cd ${SCRIPT_DIR}; git submodule update --init)
